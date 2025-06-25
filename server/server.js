c0b29dec9acfadf2eb29e19d8db088672088bb9b
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-25 22:26:53 +0530

    Implimented MongoDb config and UserModel

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import connectDB from "./configs/mongodb.js";
 
 // App Config
 const port = process.env.PORT || 4000;
 const app = express();
-
+await connectDB();
 // Initialize Middleware
 app.use(express.json());
 app.use(cors());
