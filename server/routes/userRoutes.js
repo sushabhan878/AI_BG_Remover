beac866ae5227603287994f0530e1c8f9cc9183b
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-26 11:58:57 +0530

    Implimented Usercontroller, and user Routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { clerkWebhook } from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/webhooks", clerkWebhook);
+
+export default userRouter;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 // App Config
 const port = process.env.PORT || 4000;
@@ -15,6 +16,7 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("API Working");
 });
+app.use("/api/user", userRouter);
 
 // App listen
 app.listen(port, () => {
