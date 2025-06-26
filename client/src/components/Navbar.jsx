71538ff6314771394530e24f75ae1019383ce8d8
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-26 20:59:22 +0530

    Implimented the credit into frontend including user name

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,7 +20,14 @@ const Navbar = () => {
         <img className="w-32 sm:w-44" src={assets.logo} alt="logo" />
       </Link>
       {isSignedIn ? (
-        <div>
+        <div className="flex items-center gap-2 lg:gap-3">
+          <button className="flex items-center gap-2 bg-blue-100 px-4 sm:px-7 py-1.5 sm:py-2.5 rounded-full hover:scale-105 translate-all duration-700">
+            <img className="w-5 " src={assets.credit_icon} alt="Credits Icon" />
+            <p className="text-xs sm:text-sm font-medium text-gray-600">
+              Credits: {credit}
+            </p>
+          </button>
+          <p className="text-gray-600 max-sm:hidden">Hi! {user.fullName}</p>
           <UserButton />
         </div>
       ) : (
