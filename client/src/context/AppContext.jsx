ab1a153bbb2e651cbcbca51c826202861e072a60
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-26 20:24:04 +0530

    Implimented loadCreditsData handling function

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,7 +1,32 @@
-import { createContext } from "react";
-
+import { useState, createContext } from "react";
+import { useAuth } from "@clerk/clerk-react";
+import axios from "axios";
 const AppContext = createContext();
 const AppContextProvider = (props) => {
+  /// Constants
+  const backend_url = import.meta.env.VITE_BACKEND_URL;
+
+  /// Get Data from other fils
+  const { getToken } = useAuth();
+  /// States
+  const [credit, setCredit] = useState(false);
+
+  /// Use Effects
+
+  /// Handling Function
+  const loadCreditsData = async () => {
+    try {
+      const token = await getToken();
+      const { data } = await axios.get(backend_url + "/api/user/credits", {
+        headers: { token },
+      });
+      if (data.success) {
+        setCredit(data.credits);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const value = {};
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
