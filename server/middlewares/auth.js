fb88535d6a732b7fed376385c3cd88600e36248a
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-26 20:04:22 +0530

    Implimented the clerk Middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,8 +3,20 @@ import jwt from "jsonwebtoken";
 // Middleware function to decode jwt token to get clerk id
 const authUser = async (req, res, next) => {
   try {
+    const { token } = req.headers;
+    if (!token) {
+      return res.json({
+        success: false,
+        message: "Not authorized Login Again",
+      });
+    }
+    const token_decode = jwt.decode(token);
+    req.body.clerkId = token_decode.clerkId;
+    next();
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
   }
 };
+
+export default authUser;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { clerkWebhook } from "../controllers/userController.js";
+import { clerkWebhook, userCredits } from "../controllers/userController.js";
+import authUser from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/webhooks", clerkWebhook);
-
+userRouter.get("/credits", authUser, userCredits);
 export default userRouter;
